e47683355eaf8149cd8b6bb441a1910638ab01be
knispel987 <knispel1999@gmail.com>
2022-11-21 07:39:59 -0500

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useStore } from '@/store';
+import VitailseLogo from '@/assets/logo.png';
+useHead({
+	title: 'Multimedia',
+});
+
+</script>
+
+<template>
+	<div class="">
+		<router-link :to="{ name: 'other-page' }"
+			class="mt-5 text-center hover:text-gray-200 dark:hover:text-gray-500 hover:underline">ke halaman about
+		</router-link>
+	</div>
+</template>
+
+<route lang="yaml">
+name: home
+</route>
---
+++ src/pages/other-page.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+useHead({
+	title: 'Other Pages',
+});
+</script>
+
+<template>
+	<div>
+		<p>{{ $t('pages.other.desc') }}</p>
+		<router-link :to="{ name: 'home' }" class="
+				mt-5
+				hover:text-gray-200
+				dark:hover:text-gray-500
+				hover:underline
+			">Kembali</router-link>
+	</div>
+</template>
+
+
+<route lang="yaml">
+name: other-page
+</route>
